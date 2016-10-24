6b3a7b0fdc02935bee1050d79bc40e450db62661
brandt <sebastianvbrandt@gmail.com>
2016-10-24 14:11:00 +0200

    Testen er god

--- app/src/main/java/com/example/sebastianbrandt/pushup/MainActivity.java
+++ app/src/main/java/com/example/sebastianbrandt/pushup/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
 private void TestUpLoad() {
     int i = 1+1;
-
+    int ii = 2+2;
+    int iii = i+ii;
 }
 }
 
---
+++ app/src/main/java/com/example/sebastianbrandt/pushup/test123.java
@@ -0,0 +1,8 @@
+package com.example.sebastianbrandt.pushup;
+
+/**
+ * Created by sebastianbrandt on 24/10/2016.
+ */
+
+public class test123 {
+}
